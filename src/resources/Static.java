0f6b2378224d6fe623ba1c64011c79a726cf5f30
RpprRoger <RpprRoger@googlemail.com>
2012-03-19 04:00:17 +0000

    good work 19/3

--- src/resources/Static.java
+++ src/resources/Static.java
@@ -1,14 +1,13 @@
 package resources;
 
 import java.awt.image.BufferedImage;
-import java.util.Properties;
 
 public class Static {
 	public BufferedImage src;
 	public TSI tsi;
 	public String desc;
 	
-	public Static(Properties tsi, String desc){
+	public Static(TSI tsi, String desc){
 		this.desc = desc;
 		
 	}
--- src/resources/TSI.java
+++ src/resources/TSI.java
@@ -1,41 +1,73 @@
 package resources;
 
 import java.awt.Rectangle;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Properties;
 
 public class TSI {
-	private String 	type,
+	public String 	type,
 					res,
 					regex;
-	private int w,
+	public int w,
 				h,
 				count;
-	private HashMap<String, Rectangle> charTile;
-	private ArrayList<Rectangle> mapTile;
+	private HashMap<String, Rectangle> tiles; //Contains each tile: name, bounds info
+	private HashMap<String, Integer> cha; // for characters i will store the number of each tile that corresponds to an animation
 	
 	public TSI(Properties p){
 		res = p.getProperty("system.source");
 		type = p.getProperty("system.type");
-		if(type.equals("map"))
-			map(p);
-		else if(type.equals("char"))
-			chararcter(p);
-		else
-			staticImp(p);
+		count = Integer.parseInt(p.getProperty("system.count"));
+		w = Integer.parseInt(p.getProperty("system.width"));
+		h = Integer.parseInt(p.getProperty("system.height"));
+		regex = p.getProperty("system.regex");
+		switch(res.charAt(0)){
+		case 's':
+			misc(p);
+			break;
+		case 't':
+			tile(p);
+			break;
+		case 'c':
+			character(p);
+			break;
+		}
 	}
 
-	private void chararcter(Properties p) {
-
+	private void character(Properties p) {
+		tiles = new HashMap<String, Rectangle>();
+		cha = new HashMap<String, Integer>();
+		for(int i=0, j=0;i<count;i++){
+			String name = p.getProperty(type + i + ".name");
+			Rectangle bounds = conv(p.getProperty(type + i + ".bounds"));
+			if(cha.containsKey(name))
+				j++;
+			else
+				j=0;			
+			cha.put(name, j);		
+			tiles.put(name + j, bounds);
+		}
 	}
 
-	private void map(Properties p) {
-
+	private void tile(Properties p) {
+		tiles = new HashMap<String, Rectangle>();
+		for(int i=0;i<count;i++){
+			Rectangle bounds;
+			if(p.getProperty(type + i + ".bounds").equals("0"))
+				bounds = new Rectangle(0,0,0,0);
+			else if (p.getProperty(type + i + ".bounds").equals("1"))
+				bounds = new Rectangle(0,0,w,h);
+			else
+				bounds = conv(p.getProperty(type + i + ".bounds"));
+			tiles.put(String.valueOf(i), bounds);
+		}
 	}
 	
-	private void staticImp(Properties p){
-		
+	private void misc(Properties p){
+		tiles = new HashMap<String, Rectangle>();
+		for(int i=0;i<count;i++){
+			tiles.put(p.getProperty(type + i + ".name"), conv(p.getProperty(type + i + ".bounds")));
+		}
 	}
 	
 	private Rectangle conv(String s){
@@ -48,19 +80,5 @@ public class TSI {
 				Integer.parseInt(info[2]),
 				Integer.parseInt(info[3]));
 		return r;
-	}
-	private String conv(Rectangle r){
-		String s = r.x + "#" + r.y + "#" + r.width + "#" + r.height;
-		return s;
-	}
-	
-	public Rectangle get(int i){
-		if(mapTile.size()-1<i)
-			return null;
-		else
-			return mapTile.get(i);
-	}
-	public Rectangle get(String s){
-		return null;
 	}
 }
